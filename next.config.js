71114c74369c1a7b149ecc98b03b2195641fc94d
Dennis Slavinsky <dennis.slavinsky@gmail.com>
2019-11-14 00:08:46 -0500

    fix for heroku

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
-import withCSS from '@zeit/next-css';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withCSS = require('@zeit/next-css');
 module.exports = withCSS({
     /* my next config */
 });
