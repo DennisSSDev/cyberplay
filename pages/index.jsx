7d8747276fa9559d5200b4954d8b668bdeb79b50
Dennis Slavinsky <dennis.slavinsky@gmail.com>
2019-11-16 20:59:02 -0500

    added more fixes

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,56 +1,38 @@
 // index page that next.js will use
 import React from 'react';
-import {
-    ThemeProvider,
-    createTheme,
-    Arwes,
-    Logo,
-    Words,
-    SoundsProvider,
-    createSounds,
-    Frame,
-    Button,
-    Link,
-} from 'arwes';
+import { ThemeProvider, createTheme, Arwes, Logo, Words, Frame, Button, Link } from 'arwes';
 
 import './static/index.css';
-import HeaderComponent from './components/header/component';
-
-const mySounds = {
-    shared: { volume: 1 }, // Shared sound settings
-    players: {},
-};
+import HeaderComponent from '../components/header/component';
 
 const Index = () => {
     return (
         <ThemeProvider theme={createTheme()}>
-            <SoundsProvider sounds={createSounds(mySounds)}>
-                <Arwes animate background="/cyberback.jpg">
-                    <div>
-                        <HeaderComponent />
-                    </div>
-                    <div style={{ margin: '0 auto', display: 'flex', justifyContent: 'center', paddingTop: 50 }}>
-                        <Logo animate size={130} />
-                    </div>
-                    <div style={{ margin: '0 auto', paddingLeft: '20%', paddingRight: '20%', marginTop: '5%' }}>
-                        <Frame animate level={2} corners={4}>
-                            <p style={{ marginLeft: 20 }}>
-                                <Words animate animation={{ timeout: 2000 }}>
-                                    A SciFi role-playing & texting game about finding your destiny at the world of
-                                    Cyberpunk.
-                                </Words>
-                            </p>
-                        </Frame>
-                    </div>
-                    <div style={{ display: 'flex', justifyContent: 'center', marginTop: 60 }}>
-                        <Link href="/signup">
-                            <Button level={3} animate>
-                                Begin your journey here
-                            </Button>
-                        </Link>
-                    </div>
-                </Arwes>
-            </SoundsProvider>
+            <Arwes animate background="/cyberback.jpg">
+                <div>
+                    <HeaderComponent />
+                </div>
+                <div style={{ margin: '0 auto', display: 'flex', justifyContent: 'center', paddingTop: 50 }}>
+                    <Logo animate size={130} />
+                </div>
+                <div style={{ margin: '0 auto', paddingLeft: '20%', paddingRight: '20%', marginTop: '5%' }}>
+                    <Frame animate level={2} corners={4}>
+                        <p style={{ marginLeft: 20 }}>
+                            <Words animate animation={{ timeout: 2000 }}>
+                                A SciFi role-playing & texting game about finding your destiny at the world of
+                                Cyberpunk.
+                            </Words>
+                        </p>
+                    </Frame>
+                </div>
+                <div style={{ display: 'flex', justifyContent: 'center', marginTop: 60 }}>
+                    <Link href="/signup">
+                        <Button level={3} animate>
+                            Begin your journey here
+                        </Button>
+                    </Link>
+                </div>
+            </Arwes>
         </ThemeProvider>
     );
 };
